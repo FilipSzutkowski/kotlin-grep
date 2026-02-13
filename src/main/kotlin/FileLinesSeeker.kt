c7f7c5f84b589b7c63449850d47f96c73ba82fff
FilipSzutkowski <filip99.ski@gmail.com>
2026-02-13 11:46:26 +0100

    Create a FileLineSeeker class to encapsulate reading logic

---
+++ src/main/kotlin/FileLinesSeeker.kt
@@ -0,0 +1,33 @@
+package org.example
+
+import java.io.File
+
+/**
+ * @throws [IllegalStateException] if the file does not exist, is not a file or is not readable.
+ */
+class FileLinesSeeker(val searchQuery: String, filePath: String) {
+    val gotFile = File(filePath)
+
+    init {
+        check(gotFile.exists()) { "'${gotFile.absolutePath}' does not exist" }
+        check(gotFile.isFile()) { "'${gotFile.absolutePath}' is not a file" }
+        check(gotFile.canRead()) { "'${gotFile.absolutePath}' is not readable" }
+    }
+
+    fun printLines() {
+        println()
+        gotFile.useLines {
+            println("[$gotFile]:")
+
+            val res = it.filter { str ->
+                str.contains(searchQuery, true)
+            }
+
+            res.forEach { line ->
+                println(line)
+            }
+
+        }
+    }
+
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
 package org.example
 
-import java.io.File
-
 fun main(args: Array<String>) {
     val query = args.getOrNull(0)
     val fPath = args.getOrNull(1)
@@ -11,31 +9,10 @@ fun main(args: Array<String>) {
         return
     }
 
-    val gotFile = File(fPath)
-
-    val fileValidationError = when {
-        !gotFile.exists() -> "'${gotFile.absolutePath}' does not exist"
-        !gotFile.isFile() -> "'${gotFile.absolutePath}' is not a file"
-        !gotFile.canRead() -> "'${gotFile.absolutePath}' is not readable"
-        else -> null
-    }
-
-    if (fileValidationError != null) {
-        println(fileValidationError)
-        return
-    }
-
-    gotFile.useLines {
-        println("[$gotFile]:")
-
-        val res = it.filterIndexed { i, str ->
-            str.contains(query, true)
-        }
-
-        res.forEach { line ->
-            println(line)
-        }
-
+    try {
+        FileLinesSeeker(query, fPath).printLines()
+    } catch (e: Exception) {
+        println("Error: ${e.message}")
     }
 
 }
