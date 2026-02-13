e47dec10e5c5a0a4aefcf9b9e1917fb54d9b827e
FilipSzutkowski <filip99.ski@gmail.com>
2026-02-13 08:29:44 +0100

    Open file, check if any of its lines contain the search query

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,41 @@
 package org.example
 
+import java.io.File
+
 fun main(args: Array<String>) {
-    println("Args length: ${args.size}")
-    args.forEach {
-        println("Argument: $it")
+    val query = args.getOrNull(0)
+    val fPath = args.getOrNull(1)
+
+    if (query.isNullOrBlank() || fPath.isNullOrBlank()) {
+        println("Specify search query and file path")
+        return
     }
+
+    val gotFile = File(fPath)
+
+    val fileValidationError = when {
+        !gotFile.exists() -> "'${gotFile.absolutePath}' does not exist"
+        !gotFile.isFile() -> "'${gotFile.absolutePath}' is not a file"
+        !gotFile.canRead() -> "'${gotFile.absolutePath}' is not readable"
+        else -> null
+    }
+
+    if (fileValidationError != null) {
+        println(fileValidationError)
+        return
+    }
+
+    gotFile.useLines {
+        println("[$gotFile]:")
+
+        val res = it.filterIndexed { i, str ->
+            str.contains(query, true)
+        }
+
+        res.forEach { line ->
+            println(line)
+        }
+
+    }
+
 }
